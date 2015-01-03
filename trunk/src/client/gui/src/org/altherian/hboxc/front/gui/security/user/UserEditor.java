10d06f7cfe76fa85de1449dd1f82a9917a1e1b88
noteirak <noteirak@f0167b7f-7981-408d-ab59-6ccf9e7dbb67>
2015-01-03 15:33:17 +0000

    Client/GUI - Updated text (cosmetic) Server/Security - Correctly handle username changes and handle password modification

--- trunk/src/client/gui/src/org/altherian/hboxc/front/gui/security/user/UserEditor.java
+++ trunk/src/client/gui/src/org/altherian/hboxc/front/gui/security/user/UserEditor.java
@@ -2,19 +2,19 @@
  * Hyperbox - Enterprise Virtualization Manager
  * Copyright (C) 2013 Maxime Dor
  * hyperbox at altherian dot org
- * 
+ *
  * http://hyperbox.altherian.org
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -74,11 +74,11 @@ public class UserEditor implements _Saveable, _Cancelable {
       usernameLabel.setLabelFor(usernameValue);
       
       firstPassValue = new JPasswordField();
-      firstPassLabel = new JLabel("Enter Password");
+      firstPassLabel = new JLabel("Enter New Password");
       firstPassLabel.setLabelFor(firstPassValue);
       
       secondPassValue = new JPasswordField();
-      secondPassLabel = new JLabel("Password again");
+      secondPassLabel = new JLabel("Confirm New Password");
       secondPassLabel.setLabelFor(secondPassValue);
       
       saveButton = new JButton(new SaveAction(this));
@@ -186,7 +186,6 @@ public class UserEditor implements _Saveable, _Cancelable {
             throw new HyperboxException("Password do not match");
          }
          usrIn.setPassword(firstPassValue.getPassword());
-         
       }
       
       hide();
