f3b4ce7027ce36646093099c3616108d817cd94a
Maxime Dor <max@kamax.io>
2015-03-16 23:04:52 +0100

    Add sementic versioning support

---
+++ src/altherian-tools/src/org/altherian/tool/Version.java
@@ -0,0 +1,152 @@
+/*
+ * Hyperbox - Enterprise Virtualization Manager
+ * Copyright (C) 2015 Maxime Dor
+ *
+ * http://hyperbox.altherian.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.altherian.tool;
+
+import org.altherian.tool.logging.Logger;
+
+public class Version {
+
+   private String _rawVersion = "";
+   private int _major = 0;
+   private int _minor = 0;
+   private int _patch = 0;
+   private String _revision = "";
+   
+   private boolean isValid = false;
+   private String error = "";
+   
+   public static final Version UNKNOWN = new Version("0.0.0");
+   
+   public Version(String rawVersion) {
+      if (AxStrings.isEmpty(rawVersion)) {
+         isValid = false;
+         error = "Version cannot be null or empty";
+         return;
+      }
+      _rawVersion = rawVersion;
+
+      String[] mainSplit = rawVersion.split("-");
+      if (mainSplit.length > 1) {
+         _revision = mainSplit[1];
+      }
+      
+      String[] versionSplit = mainSplit[0].split("\\.", 3);
+      if (versionSplit.length != 3) {
+         isValid = false;
+         error = "Version is not in x.y.z format";
+         return;
+      }
+      
+      try {
+         _major = Integer.parseInt(versionSplit[0]);
+         _minor = Integer.parseInt(versionSplit[1]);
+         _patch = Integer.parseInt(versionSplit[2]);
+         isValid = true;
+      } catch (Throwable e) {
+         isValid = false;
+         error = e.getMessage();
+         Logger.exception(e);
+      }
+   }
+   
+   public boolean isValid() {
+      return isValid;
+   }
+   
+   public String getError() {
+      return error;
+   }
+
+   public void validate() {
+      if (!isValid()) {
+         throw new RuntimeException("Invalid version: " + _rawVersion);
+      }
+   }
+   
+   public boolean isCompatible(String otherVersion) {
+      return isCompatible(new Version(otherVersion));
+   }
+   
+   public boolean isCompatible(Version otherVersion) {
+      return (otherVersion != null) && otherVersion.isValid() && isValid() && (getMajor() == otherVersion.getMajor())
+            && (getMinor() >= otherVersion.getMinor());
+   }
+   
+   public int getMajor() {
+      validate();
+      return _major;
+   }
+
+   public int getMinor() {
+      validate();
+      return _minor;
+   }
+   
+   public int getPatch() {
+      validate();
+      return _patch;
+   }
+   
+   public String getRevision() {
+      validate();
+      return _revision;
+   }
+   
+   @Override
+   public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = (prime * result) + ((_rawVersion == null) ? 0 : _rawVersion.hashCode());
+      return result;
+   }
+   
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+         return true;
+      }
+      if (obj == null) {
+         return false;
+      }
+      if (!(obj instanceof Version)) {
+         return false;
+      }
+      Version other = (Version) obj;
+      if (_rawVersion == null) {
+         if (other._rawVersion != null) {
+            return false;
+         }
+      } else if (!_rawVersion.equals(other._rawVersion)) {
+         return false;
+      }
+      return true;
+   }
+   
+   @Override
+   public String toString() {
+      return _rawVersion;
+   }
+
+   public String getFormated() {
+      return _major + "." + _minor + "." + _patch + (AxStrings.isEmpty(_revision) ? "" : "-" + _revision);
+   }
+   
+}
---
+++ src/altherian-tools/test/org/altherian/tool/VersionTest.java
@@ -0,0 +1,64 @@
+/*
+ * Hyperbox - Enterprise Virtualization Manager
+ * Copyright (C) 2015 Maxime Dor
+ *
+ * http://hyperbox.altherian.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.altherian.tool;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class VersionTest {
+
+   private final static String v1_0_0 = "1.0.0";
+   private final static String v2_3_4 = "2.3.4";
+   private final static String v3_4_5r67 = "3.4.5-67";
+   private final static String invalid1 = "invalidversion";
+   private final static String invalid2 = "8.9-0";
+   private final static String invalid3 = "";
+   private final static String invalid4 = null;
+
+   @Test
+   public void unknown() {
+      Version unknown = new Version(Version.UNKNOWN.toString());
+      assertTrue(Version.UNKNOWN.equals(unknown));
+   }
+   
+   @Test
+   public void valid() {
+      assertTrue(Version.UNKNOWN.isValid());
+      assertTrue(new Version(v1_0_0).isValid());
+      assertTrue(new Version(v2_3_4).isValid());
+      assertTrue(new Version(v3_4_5r67).isValid());
+
+      assertFalse(new Version(invalid1).isValid());
+      assertFalse(new Version(invalid2).isValid());
+      assertFalse(new Version(invalid3).isValid());
+      assertFalse(new Version(invalid4).isValid());
+   }
+   
+   @Test
+   public void equal() {
+      assertTrue(Version.UNKNOWN.equals(Version.UNKNOWN));
+      assertTrue((new Version(v1_0_0)).equals(new Version(v1_0_0)));
+      assertTrue((new Version(v2_3_4)).equals(new Version(v2_3_4)));
+      assertTrue((new Version(v3_4_5r67)).equals(new Version(v3_4_5r67)));
+   }
+   
+}
